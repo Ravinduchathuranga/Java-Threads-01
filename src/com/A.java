b7ee813cca41dcc47f75641062394cb413c3e419
Ravinduchathuranga <ravicdasanayaka@gmail.com>
2024-03-20 18:14:50 +0530

    run method added

--- src/com/A.java
+++ src/com/A.java
@@ -22,8 +22,18 @@ public class A {
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
-
+                account.setBalance(2000);
             }
         });
+        
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(account.getBalance());
+            }
+        });
+        
+        t.start();
+        t1.start();
     }
 }
