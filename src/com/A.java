bf42954e0994937bb126f0f029ce0cb361cfccc3
Ravinduchathuranga <ravicdasanayaka@gmail.com>
2024-03-20 18:58:10 +0530

    new changers added

--- src/com/A.java
+++ src/com/A.java
@@ -4,11 +4,11 @@ class Account {
 
     private double balance;
 
-    public double getBalance() {
+    public synchronized double getBalance() {
         return balance;
     }
 
-    public void setBalance(double balance) {
+    public synchronized void setBalance(double balance) {
         for (int i = 0; i < 10000; i++) {
             this.balance++;
         }
@@ -19,17 +19,16 @@ public class A {
 
     public static void main(String[] args) {
         Account account = new Account();
-
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
-                account.setBalance(2000);               
+                account.setBalance(100000);
             }
         });
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println(account.getBalance());
+                System.out.println("Balance :" + account.getBalance());
             }
         });
         t.start();
